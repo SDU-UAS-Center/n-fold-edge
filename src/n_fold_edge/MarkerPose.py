746cb8879e5da0a535795acae65d99c701c49fed
Henrik Dyrberg Egemose <hde@mmmi.sdu.dk>
2026-04-08 11:04:35 +0200

    add type annotate to marker pose

--- src/n_fold_edge/MarkerPose.py
+++ src/n_fold_edge/MarkerPose.py
@@ -1,19 +1,16 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue May 13 19:38:43 2014
-
-@author: henrik
-"""
+"""Define marker position and orientation."""
 
 
 class MarkerPose:
-    def __init__(self, x, y, theta, quality, order=None):
+    """Define Marker Position and Orientation."""
+
+    def __init__(self, x: float, y: float, theta: float, quality: float, order: int | None = None) -> None:
         self.x = x
         self.y = y
         self.theta = theta
         self.quality = quality
         self.order = order
 
-    def scale_position(self, scale_factor):
+    def scale_position(self, scale_factor: float) -> None:
         self.x = self.x * scale_factor
         self.y = self.y * scale_factor
