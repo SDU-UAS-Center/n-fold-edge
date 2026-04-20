f5c8f5985a85ff99c60717eccdcfaf5c554c439d
Henrik Dyrberg Egemose <hde@mmmi.sdu.dk>
2026-04-20 12:00:05 +0200

    add string repesentaion and option to return values as list

--- src/n_fold_edge/MarkerPose.py
+++ src/n_fold_edge/MarkerPose.py
@@ -4,7 +4,7 @@
 class MarkerPose:
     """Define Marker Position and Orientation."""
 
-    def __init__(self, x: float, y: float, theta: float, quality: float, order: int | None = None) -> None:
+    def __init__(self, x: float, y: float, theta: float, quality: float, order: int) -> None:
         self.x = x
         self.y = y
         self.theta = theta
@@ -14,3 +14,11 @@ class MarkerPose:
     def scale_position(self, scale_factor: float) -> None:
         self.x = self.x * scale_factor
         self.y = self.y * scale_factor
+
+    def __str__(self) -> str:
+        """Return string representation."""
+        return f"x: {self.x:.2f}, y: {self.y:.2f}, theta: {self.theta:.2f}, quality: {self.quality:.2f}, order: {self.order:.2f}"
+
+    def as_list(self) -> list[float | int]:
+        """Return a list representation."""
+        return [self.x, self.y, self.theta, self.quality, self.order]
