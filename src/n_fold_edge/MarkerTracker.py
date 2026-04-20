6841e6435f43f9db694af169e6c8bd83b93e0a00
Henrik Dyrberg Egemose <hde@mmmi.sdu.dk>
2026-04-20 12:04:12 +0200

    add cli for locator and update tracker to handle different types of video

--- src/n_fold_edge/MarkerTracker.py
+++ src/n_fold_edge/MarkerTracker.py
@@ -1,10 +1,11 @@
 """Main file for N-fold-edge."""
 
+import csv
 import math
-import os
 import signal
-from collections.abc import Iterable
-from time import strftime, time
+from collections.abc import Sequence
+from pathlib import Path
+from time import strftime
 from typing import Any
 
 import cv2
@@ -13,180 +14,110 @@ import numpy as np
 from .MarkerLocator import MarkerLocator
 from .MarkerPose import MarkerPose
 
-# parameters
-print_debug_messages = False
-show_image = True
-list_of_markers_to_find = [4, 5]
-get_images_to_flush_cam_buffer = 5
 
-# global variables
-stop_flag = False
+class MarkerTracker:
+    """Track markers in video file or video stream."""
 
+    def __init__(self, marker_locators: MarkerLocator | Sequence[MarkerLocator], show_video: bool = False) -> None:
+        if isinstance(marker_locators, Sequence):
+            self.marker_locators = marker_locators
+        else:
+            self.marker_locators = (marker_locators,)
+        self.show_video = show_video
+        self.stop_flag = False
+        signal.signal(signal.SIGINT, self.signal_handler)
 
-# define ctrl-c handler
-def signal_handler(signal: int, frame: Any) -> None:
-    """Handle signal to terminate program."""
-    global stop_flag
-    stop_flag = True
+    def signal_handler(self, signal: int, frame: Any) -> None:
+        self.stop_flag = True
 
-
-# install ctrl-c handler
-signal.signal(signal.SIGINT, signal_handler)
-
-
-def set_camera_focus() -> None:
-    """Set Camera Focus."""
-    # Disable autofocus
-    os.system("v4l2-ctl -d 1 -c focus_auto=0")
-
-    # Set focus to a specific value. High values for nearby objects and
-    # low values for distant objects.
-    os.system("v4l2-ctl -d 1 -c focus_absolute=0")
-
-    # sharpness (int)    : min=0 max=255 step=1 default=128 value=128
-    os.system("v4l2-ctl -d 1 -c sharpness=200")
-
-
-class CameraDriver:
-    """
-    Purpose: capture images from a camera and delegate processing of the
-    images to a different class.
-    """
-
-    def __init__(
-        self,
-        marker_orders: Iterable[int] = (6,),
-        default_kernel_size: int = 21,
-        scaling_parameter: float = 2500,
-        downscale_factor: float = 1,
-    ) -> None:
-        # Initialize camera driver.
-        # Open output window.
-        if show_image is True:
-            cv2.namedWindow("filterdemo", cv2.WINDOW_AUTOSIZE)
-
-        # Select the camera where the images should be grabbed from.
-        set_camera_focus()
-        self.camera = cv2.VideoCapture(0)
-        self.set_camera_resolution()
-
-        # Storage for image processing.
-        self.current_frame: np.ndarray
-        self.processed_frame: np.ndarray
-        self.running = True
-        self.downscale_factor = downscale_factor
-
-        # Storage for trackers.
-        self.trackers: list[MarkerLocator] = []
-        self.old_locations: list[MarkerPose] = []
-
-        # Initialize trackers.
-        for marker_order in marker_orders:
-            temp = MarkerLocator(marker_order, default_kernel_size, scaling_parameter)
-            temp.track_marker_with_missing_black_leg = False
-            self.trackers.append(temp)
-            self.old_locations.append(MarkerPose(0, 0, 0, 0, 0))
-
-    def set_camera_resolution(self) -> None:
-        self.camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-        self.camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
-
-    def get_image(self) -> None:
-        # Get image from camera.
-        for _ in range(get_images_to_flush_cam_buffer):
-            self.current_frame = self.camera.read()[1]
-
-    def process_frame(self) -> None:
-        self.processed_frame = self.current_frame
-        # Locate all markers in image.
-        frame_gray = cv2.cvtColor(self.current_frame, cv2.COLOR_RGB2GRAY)
-        reduced_image = cv2.resize(
-            frame_gray,
-            (0, 0),
-            fx=1.0 / self.downscale_factor,
-            fy=1.0 / self.downscale_factor,
-        )
-        for k in range(len(self.trackers)):
-            # Previous marker location is unknown, search in the entire image.
-            self.old_locations[k] = self.trackers[k].locate_marker(reduced_image)
-            self.old_locations[k].scale_position(self.downscale_factor)
-
-    def draw_detected_markers(self) -> None:
-        if self.processed_frame is not None:
-            for k in range(len(self.trackers)):
-                xm = int(self.old_locations[k].x)
-                ym = int(self.old_locations[k].y)
-                orientation = self.old_locations[k].theta
-                if self.old_locations[k].quality < 0.9:
-                    cv2.circle(self.processed_frame, (xm, ym), 4, (55, 55, 255), 1)
-                else:
-                    cv2.circle(self.processed_frame, (xm, ym), 4, (55, 55, 255), 3)
-
-                xm2 = int(xm + 50 * math.cos(orientation))
-                ym2 = int(ym + 50 * math.sin(orientation))
-                cv2.line(self.processed_frame, (xm, ym), (xm2, ym2), (255, 0, 0), 2)
-
-    def show_processed_frame(self) -> None:
-        if show_image is True and self.processed_frame is not None:
-            cv2.imshow("filterdemo", self.processed_frame)
-
-    def reset_all_locations(self) -> None:
-        # Reset all markers locations, forcing a full search on the next iteration.
-        for k in range(len(self.trackers)):
-            self.old_locations[k] = MarkerPose(0, 0, 0, 0, 0)
-
-    def handle_keyboard_events(self) -> None:
-        if show_image is True:
+    def handle_keyboard_events(self, frame: np.ndarray) -> bool:
+        if self.stop_flag:
+            return True
+        if self.show_video is True:
             # Listen for keyboard events and take relevant actions.
-            key = cv2.waitKey(100)
+            key = cv2.waitKey(1)
             # Discard higher order bit, http://permalink.gmane.org/gmane.comp.lib.opencv.devel/410
             key = key & 0xFF
-            if key == 27:  # Esc
-                self.running = False
-            if key == 114:  # R
-                print("Resetting")
-                self.reset_all_locations()
-            if key == 115:  # S
+            if key == 27 or key == 113:  # Esc/q
+                return True
+            elif key == 115:  # S
                 # save image
                 print("Saving image")
                 filename = strftime("%Y-%m-%d %H-%M-%S")
-                if self.current_frame is not None:
-                    cv2.imwrite(f"output/{filename}.png", self.current_frame)
+                cv2.imwrite(f"output/{filename}.png", frame)
+        return False
 
-    def return_positions(self) -> list[MarkerPose]:
-        # Return list of all marker locations.
-        return self.old_locations
+    def draw_detected_markers(self, frame: np.ndarray, marker_pose: MarkerPose) -> np.ndarray:
+        xm = int(marker_pose.x)
+        ym = int(marker_pose.y)
+        orientation = marker_pose.theta
+        if marker_pose.quality < 0.9:
+            cv2.circle(frame, (xm, ym), 4, (55, 55, 255), 1)
+        else:
+            cv2.circle(frame, (xm, ym), 4, (55, 55, 255), 3)
+        xm2 = int(xm + 50 * math.cos(orientation))
+        ym2 = int(ym + 50 * math.sin(orientation))
+        cv2.line(frame, (xm, ym), (xm2, ym2), (255, 0, 0), 2)
+        return frame
 
+    def prepare_csv_file(self, csv_file_path: Path) -> None:
+        if csv_file_path.exists():
+            raise FileExistsError("csv file already exist. Choose another filename.")
+        with open(csv_file_path, "a") as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow(["Frame", "x", "y", "direction", "quality", "order"])
 
-def main() -> None:
-    """Main function for running n-fold-edge on video."""
-    cd = CameraDriver(
-        list_of_markers_to_find,
-        default_kernel_size=55,
-        scaling_parameter=1000,
-        downscale_factor=1,
-    )  # Best in robolab.
-    # cd = ImageDriver(list_of_markers_to_find, defaultKernelSize = 21)
-    t0 = time()
+    def save_marker_to_csv(self, csv_file_path: Path, marker_pose: MarkerPose, frame_number: float) -> None:
+        with open(csv_file_path, "a") as csv_file:
+            writer = csv.writer(csv_file)
+            writer.writerow([frame_number] + marker_pose.as_list())
 
-    while cd.running and stop_flag is False:
-        (t1, t0) = (t0, time())
-        if print_debug_messages is True:
-            print("time for one iteration: %f" % (t0 - t1))
-        cd.get_image()
-        cd.process_frame()
-        cd.draw_detected_markers()
-        cd.show_processed_frame()
-        cd.handle_keyboard_events()
-        y = cd.return_positions()
-        for k in range(len(y)):
-            try:
-                # pose_corrected = perspective_corrector.convertPose(y[k])
-                pose_corrected = y[k]
-                print(
-                    f"{pose_corrected.x:8.3f} {pose_corrected.y:8.3f} {pose_corrected.theta:8.3f} {pose_corrected.quality:8.3f} {pose_corrected.order}"
-                )
-            except Exception as e:
-                print(e)
-
-    print("Stopping")
+    def track(self, video: Path | int, save_video_path: Path | None = None, save_csv_path: Path | None = None) -> None:
+        cap = cv2.VideoCapture(video)
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+        if save_csv_path is not None:
+            self.prepare_csv_file(save_csv_path)
+        if save_video_path is not None:
+            if isinstance(video, int):
+                fourcc = cv2.VideoWriter.fourcc(*"DIVX")
+                if save_video_path.suffix != ".avi":
+                    raise OSError("Only video with avi extension is supported as output.")
+            else:
+                fourcc = int(cap.get(cv2.CAP_PROP_FOURCC))
+            fps = cap.get(cv2.CAP_PROP_FPS)
+            frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+            frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            video_out = cv2.VideoWriter(save_video_path, fourcc, fps, (frame_width, frame_height))
+        else:
+            video_out = None
+        if not cap.isOpened():
+            raise OSError(f"Could not open video {video}.")
+        internal_frame_number = 0
+        if self.show_video:
+            cv2.namedWindow("Marker Tracker", cv2.WINDOW_AUTOSIZE)
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+            frame_gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+            for ml in self.marker_locators:
+                marker_pose = ml.locate_marker(frame_gray)
+                frame = self.draw_detected_markers(frame, marker_pose)
+                if save_csv_path is not None:
+                    frame_number = cap.get(cv2.CAP_PROP_POS_FRAMES)
+                    if frame_number < 0:
+                        frame_number = internal_frame_number
+                    self.save_marker_to_csv(save_csv_path, marker_pose, frame_number)
+            if self.show_video:
+                cv2.imshow("Marker Tracker", frame)
+            if video_out is not None:
+                video_out.write(frame)
+            stop = self.handle_keyboard_events(frame)
+            if stop:
+                break
+            internal_frame_number += 1
+        if video_out is not None:
+            video_out.release()
+        cap.release()
+        cv2.destroyAllWindows()
--- src/n_fold_edge/__init__.py
+++ src/n_fold_edge/__init__.py
@@ -1,6 +1,82 @@
-from .MarkerTracker import main as ML_main
+import argparse
+from pathlib import Path
+
+import cv2
+import numpy as np
+
+from .MarkerLocator import MarkerLocator
+from .MarkerTracker import MarkerTracker
 
 
-def main() -> None:
-    """Main function running n-fold-edge as script."""
-    ML_main()
+def video_type(string: str) -> int | Path:
+    """Determine video type for argparse."""
+    try:
+        return int(string)
+    except ValueError:
+        return Path(string)
+
+
+def track_markers() -> None:
+    """Function for cmd to track markers in video."""
+    parser = argparse.ArgumentParser("track-markers", description="Track Markers in Video.")
+    parser.add_argument(
+        "video", type=video_type, help="Video on which to track markers. Or an id to capture video from."
+    )
+    parser.add_argument("--order", type=int, nargs="*", default=5, help="Order of Markers to Track.")
+    parser.add_argument("--kernel-size", type=int, default=55, help="Kernel size to use for marker location")
+    parser.add_argument("--scale-factor", type=float, default=1000, help="Scale factor to scale video by.")
+    parser.add_argument("-o", "--output", type=Path, help="Location to csv file with marker location for each frame.")
+    parser.add_argument("--output-video", type=Path, help="Location to save video with marker location drawn on.")
+    parser.add_argument("--show", action="store_true", help="Show Video with located markers.")
+    args = parser.parse_args()
+    marker_locators = tuple(MarkerLocator(order, args.kernel_size, args.scale_factor) for order in args.order)
+    mt = MarkerTracker(marker_locators, show_video=args.show)
+    mt.track(args.video, save_video_path=args.output_video, save_csv_path=args.output)
+
+
+def locate_markers() -> None:
+    """Function for locating markers in image."""
+    parser = argparse.ArgumentParser("locate-markers", description="Locate Markers in image.")
+    parser.add_argument("image", type=Path, help="Image on which to locate markers. Or folder with images.")
+    parser.add_argument("--order", type=int, nargs="*", default=5, help="Order of Markers to locate.")
+    parser.add_argument("--kernel-size", type=int, default=55, help="Kernel size to use for marker location")
+    parser.add_argument("--scale-factor", type=float, default=1000, help="Scale factor to scale image by.")
+    parser.add_argument("-o", "--output", type=Path, help="Location to save image with markers drawn on.")
+    args = parser.parse_args()
+    if args.image.is_dir():
+        image_files = []
+        image_files.extend(args.image.glob("**/*.png"))
+        image_files.extend(args.image.glob("**/*.jpg"))
+        image_files.extend(args.image.glob("**/*.jpeg"))
+    else:
+        image_files = [args.image]
+    if len(image_files) > 1 and not args.output.is_dir():
+        print("When input is multiple images output must be a directory!")
+        return
+    for image_file in image_files:
+        image = cv2.imread(image_file)
+        if image is None:
+            print("Image not found!")
+            continue
+        gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        for order in args.order:
+            ml = MarkerLocator(order, args.kernel_size, args.scale_factor)
+            marker_pose = ml.locate_marker(gray_image)
+            print(marker_pose)
+            if args.output is not None:
+                xm = int(marker_pose.x)
+                ym = int(marker_pose.y)
+                orientation = marker_pose.theta
+                if marker_pose.quality < 0.9:
+                    cv2.circle(image, (xm, ym), 4, (55, 55, 255), 1)
+                else:
+                    cv2.circle(image, (xm, ym), 4, (55, 55, 255), 3)
+                xm2 = int(xm + 50 * np.cos(orientation))
+                ym2 = int(ym + 50 * np.sin(orientation))
+                cv2.line(image, (xm, ym), (xm2, ym2), (255, 0, 0), 2)
+        if args.output is not None:
+            if args.output.is_dir():
+                out_file = args.output.joinpath(image_file.name)
+            else:
+                out_file = args.output
+            cv2.imwrite(out_file, image)
